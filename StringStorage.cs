71f368ef632dbf8790b924cd4cd080affd464c33
herboren <herboren@users.noreply.github.com>
2021-06-13 03:05:34 -0400

    Add files via upload

---
+++ StringStorage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IntegralHashing
+{
+    public class StringStorage
+    {
+        // Store imported hashes        
+        public Dictionary<string, string> keyVal {get; set;}
+
+        public Dictionary<string, string> matched { get; set; }
+
+        public Dictionary<string, string> unique { get; set; }
+
+        // Stores hashed files for comparing to list
+        public List<string> hashed = new List<string>();
+
+        // Stores files paths obtained from directory
+        public List<string> absolute = new List<string>();
+
+        // Stores the directory 
+        public string directory { get; set; }
+
+        
+    }
+}
